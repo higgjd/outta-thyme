d0aeacc8663b8099b2abfaafe5b25172e581e5ef
Jenny Higgins <higgjd@gmail.com>
2022-06-02 17:12:49 +0100

    added routes for recipes and favourites, as well as generating the recipes controller and view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :recipes, only: [:show, :index] do
+    resources :favourites, only: [:index, :destroy, :new, :create ]
+  end
 end
