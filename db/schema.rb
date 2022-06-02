ee1466e8117ee40ec7243726e34b62b9649fa077
Jenny Higgins <higgjd@gmail.com>
2022-06-02 17:00:58 +0100

    created favourite model and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_02_155509) do
+ActiveRecord::Schema.define(version: 2022_06_02_160012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "recipe_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_favourites_on_recipe_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -37,4 +46,6 @@ ActiveRecord::Schema.define(version: 2022_06_02_155509) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favourites", "recipes"
+  add_foreign_key "favourites", "users"
 end
