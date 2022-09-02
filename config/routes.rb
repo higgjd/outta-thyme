a3ba17344b96da49153d4675365afdb26aaa2db0
Jenny Higgins <higgjd@gmail.com>
2022-09-02 14:57:41 +0100

    added basic styling and links to homepage and navbar, and sticky footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'favourites/index'
-  get 'favourites/new'
-  get 'favourites/create'
-  get 'favourites/destroy'
-  get 'recipes/index'
-  get 'recipes/show'
+  # get 'favourites/index'
+  # get 'favourites/new'
+  # get 'favourites/create'
+  # get 'favourites/destroy'
+  # get 'recipes/index'
+  # get 'recipes/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
